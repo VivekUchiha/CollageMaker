045d3ffd5bccd23dac5ddb30b044cc5e201e1f4b
vivekuchiha <satsvivek@gmail.com>
2019-04-06 14:58:31 +0530

    Initial

---
+++ app/src/main/java/com/example/collagemaker/VerticalPageTransformer.java
@@ -0,0 +1,46 @@
+package com.example.collagemaker;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+
+public class VerticalPageTransformer implements ViewPager.PageTransformer {
+    private float MIN_SCALE = 0.75f;
+
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+
+        if (position < -1) {
+
+            view.setAlpha(0);
+
+
+        }
+        else if (position <= 0) {
+
+            view.setAlpha(1);
+
+            view.setScaleX(1);
+            view.setScaleY(1);
+            float yPosition = position * view.getHeight();
+            view.setTranslationY(yPosition);
+            view.setTranslationX(-1 * view.getWidth() * position);
+
+        } else if (position <= 1) {
+
+            view.setAlpha(1 - position);
+
+            view.setTranslationX(-1 * view.getWidth() * position);
+
+
+
+        }
+
+        else {
+
+            view.setAlpha(0);
+        }
+
+    }
+
+}
