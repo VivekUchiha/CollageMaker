1a63bd65b3a425a82653b453d055d961006be7c7
vivekuchiha <satsvivek@gmail.com>
2019-04-06 18:53:36 +0530

    Initial

--- app/src/main/java/com/example/collagemaker/MainActivity.java
+++ app/src/main/java/com/example/collagemaker/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.collagemaker;
 
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity{
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        {
+            getSupportFragmentManager().beginTransaction().add(R.id.fra, new CameraFragment()).commit();
+        }
     }
 }
